9f69231362929235662f8efbb4096072e9e3bf0c
gtr77 <yecid.gustavo.x.romero.higuera@disney.com>
2020-12-15 01:08:11 -0500

    Search functionalily and Props

--- src/App.js
+++ src/App.js
@@ -8,22 +8,25 @@ import "./App.css";
 class App extends Component {
 	state = {
 		users: [],
-		loading: false
-	}
-	async componentDidMount() {
+		loading: false,
+	};
+
+	searchUsers = async (text) => {
 		this.setState({ loading: true });
-		const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
 		console.log(res.data);
-		this.setState({ loading: false, users: res.data });
+		this.setState({ loading: false, users: res.data.items });
 	}
 
 	render() {
 		return (
 			<div className="App">
 				<Navbar />
-				<Search />
+				<Search searchUsers={this.searchUsers} />
 				<div className="container">
-					<Users loading={this.state.loading} users={this.state.users}/>
+					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
 		);
