c4317fa4874cc613243579b4543bfe1d2da5c2aa
Yecid Gustavo <yecid.gustavo.x.romero.higuera.-nd@disney.com>
2021-07-03 02:12:49 -0500

    React View - Start up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = "John Doe"
+    let isLoading = true
+
+    return (
+      <div className="App"> 
+        <h2>{isLoading ? 'Loading...': null}</h2>
+        <h1>Hellow and Welcome { name }</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
